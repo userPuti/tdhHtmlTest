2505338a56288e5512c6a678341337640ce166ee
Puti <349251265@qq.com>
2022-03-11 15:37:03 +0800

    3/11

--- tdhHtmlTest/js/t1.js
+++ tdhHtmlTest/js/t1.js
@@ -1,3 +1,14 @@
+$(function () {
+    $("#add").click(function () {
+        console.log(1);
+        window.open("t2.html");
+        console.log(2);
+        addLine();
+        console.log(3);
+    })
+})
+
+
 function showInfo() {
     let yhxm = document.getElementById("yhxm").value;
     let index = document.getElementById('yhbm').selectedIndex;
@@ -5,23 +16,26 @@ function showInfo() {
     alert(encoderStr(yhxm) + "   " + encoderStr(yhbm[index].text));
 }
 
+
 function encoderStr(str) {
     return encodeURI(str);
 }
+
 
 function deleteInfo(obj) {
     let tr = obj.parentNode.parentNode;
     tr.parentNode.removeChild(tr);
 }
 
+
 function jumpToInfoPage() {
     window.open('t2.html');
 }
 
+
 function loadInfo(obj) {
     let tbody = document.getElementById("tableId");
     // let rows = tbody.rows;
-
     let xh = obj.parentNode.parentNode.children[0].innerHTML;
 
     let name, yhkl, yhbm, xb, pxh, sfjy;
@@ -37,5 +51,72 @@ function loadInfo(obj) {
     window.open("t2.html?pxh=" + encodeURIComponent(pxh) + "&name=" + encodeURIComponent(name) +
         "&xh=" + encodeURIComponent(xh) + "&yhkl=" + encodeURIComponent(yhkl) + "&yhbm=" + encodeURIComponent(yhbm) +
         "&xb=" + encodeURIComponent(xb) + "&sfjy=" + encodeURIComponent(sfjy) + "&csrq=" + encodeURIComponent(csrq));
+}
+
+
+function getQueryString(name) {
+    let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    let r = window.location.search.substr(1).match(reg);
+    if (r != null) {
+        return decodeURIComponent(r[2]);
+    };
+    return null;
+}
+
+
+function getUserInfo() {
+    let xh = getQueryString("xh");
+    let pxh = getQueryString("pxh");
+    let name = getQueryString("name");
+    let yhkl = getQueryString("yhkl");
+    let yhbm = getQueryString("yhbm");
+    let xb = getQueryString("xb");
+    let sfjy = getQueryString("sfjy");
+    let csrq = getQueryString("csrq");
+
+    return {
+        xh: xh,
+        pxh: pxh,
+        name: name,
+        yhkl: yhkl,
+        yhbm: yhbm,
+        xb: xb,
+        sfjy: sfjy,
+        csrq: csrq
+    }
+}
+
+function addLine() {
+    let user = getUserInfo();
+    console.log(getUserInfo());
+
+    if (user.xh == null || user.pxh == null || user.name == null || user.yhkl == null || user.yhbm == null || user.xb == null || user.sfjy == null || user.csrq == null) {
+        retun;
+    }
+
+    let line = $("#firstLine").clone();
+    $("tbody").append(line);
+    $("tbody tr:last").find("td").html('');
+    let i = 1;
+    $(".xh").each(function () {
+        $(this).html(i++);
+    }) 
+
+    $("tbody tr:last").find("td").eq(1).html(user.name);
+    $("tbody tr:last").find("td").eq(2).html(user.yhkl);
+    $("tbody tr:last").find("td").eq(3).html(user.yhbm);
+    $("tbody tr:last").find("td").eq(4).html(user.xb);
+    $("tbody tr:last").find("td").eq(5).html(user.pxh);
+    if(user.sfjy == "on") {
+        $("tbody tr:last").find("td").eq(6).html("是");
+    } else {
+        $("tbody tr:last").find("td").eq(6).html("否");
+    }
+    $("tbody tr:last").find("td").eq(7).html(user.csrq);
+
+    $("tbody tr:last").find("td").eq(8).html('<img src="./查看.png" width="30" height="30" onclick="loadInfo(this)" style="cursor:pointer" />');
+    $("tbody tr:last").find("td").eq(9).html('<img src="./删除.png" width="30" height="30" onclick="deleteInfo(this)" style="cursor:pointer" />');
+    $("tbody tr:last").find("td").eq(10).html('<img src="./修改.png" width="30" height="30" onclick="window.open(\'./t2.html\')" style="cursor:pointer" />');
+
 
 }
--- tdhHtmlTest/js/t2.js
+++ tdhHtmlTest/js/t2.js
@@ -1,8 +1,135 @@
+$(function () {
+    //checkInfo();
+    // modifier();
+   
+    $("#back").click(function () {
+        window.close();
+    });
+    
+    $("#submit").click(function () {
+        validateForm();
+        addInfo();
+    })
+})
+
+
+function getUserInfo() {
+    let xh = getQueryString("xh");
+    let pxh = getQueryString("pxh");
+    let name = getQueryString("name");
+    let yhkl = getQueryString("yhkl");
+    let yhbm = getQueryString("yhbm");
+    let xb = getQueryString("xb");
+    let sfjy = getQueryString("sfjy");
+    let csrq = getQueryString("csrq");
+
+    return {
+        xh: xh,
+        pxh: pxh,
+        name: name,
+        yhkl: yhkl,
+        yhbm: yhbm,
+        xb: xb,
+        sfjy: sfjy,
+        csrq: csrq
+    }
+}
+
+function checkInfo() {
+    let user = getUserInfo();
+    console.log(getUserInfo());
+
+    if (user.xh == null || user.pxh == null || user.name == null || user.yhkl == null || user.yhbm == null || user.xb == null || user.sfjy == null || user.csrq == null) {
+        retun;
+    }
+
+    $("#yhidText").parent().html(user.xh);
+
+    $("#pxhText").parent().html(user.pxh);
+    $("#yhxmText").parent().html(user.name);
+
+    $("#yhklText").parent().html(user.yhkl);
+    $("#cfklText").parent().html(user.yhkl);
+
+    $('#yhbm').hide();
+    $("#yhbm").parent().html(user.yhbm);
+
+    $('#xb').hide();
+    $("#xb").parent().html(user.xb);
+
+    if (user.sfjy == '否') {
+        $('#sfjy').hide();
+        $('#sfjy').parent().html('否');
+    } else {
+        $('#sfjy').hide();
+        $('#sfjy').parent().html('是');
+    }
+
+    $('#date').hide();
+    $("#date").parent().html(user.csrq);
+
+    $('#submit').hide();
+}
+
+function getQueryString(name) {
+    let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    let r = window.location.search.substr(1).match(reg);
+    if (r != null) {
+        return decodeURIComponent(r[2]);
+    };
+    return null;
+}
+
+function modifier() {
+    let user = getUserInfo();
+    console.log(getUserInfo());
+
+    if (user.xh == null || user.pxh == null || user.name == null || user.yhkl == null || user.yhbm == null || user.xb == null || user.sfjy == null || user.csrq == null) {
+        retun;
+    }
+
+    $("#yhidText").parent().html(user.xh);
+
+    $("#pxhText").parent().val(user.pxh);
+    $("#yhxmText").parent().val(user.name);
+
+    $("#yhklText").parent().val(user.yhkl);
+    $("#cfklText").parent().val(user.yhkl);
+
+    window.open("t1.html?pxh=" + encodeURIComponent(user.pxh) + "&name=" + encodeURIComponent(user.name) +
+        "&xh=" + encodeURIComponent(user.xh) + "&yhkl=" + encodeURIComponent(user.yhkl) + "&yhbm=" + encodeURIComponent(user.yhbm) +
+        "&xb=" + encodeURIComponent(user.xb) + "&sfjy=" + encodeURIComponent(user.sfjy) + "&csrq=" + encodeURIComponent(user.csrq));
+}
+
+function addInfo() {
+    let xh = $("#yhidText").val();
+    // console.log(xh);
+    let pxh = $("#pxhText").val();
+    // console.log(pxh);
+    let name = $("#yhxmText").val();
+    let yhkl = $("#yhklText").val();
+    let yhbm = $("#yhbm option:selected").text();
+    let xb = $("#xb option:selected").text();
+    let sfjy = $("input[type=checkbox]:checked").val();
+    // console.log(sfjy);
+    let csrq =$("#date").val();
+
+    console.log(csrq);
+    window.open("t1.html?pxh=" + encodeURIComponent(pxh) + "&name=" + encodeURIComponent(name) +
+        "&xh=" + encodeURIComponent(xh) + "&yhkl=" + encodeURIComponent(yhkl) + "&yhbm=" + encodeURIComponent(yhbm) +
+        "&xb=" + encodeURIComponent(xb) + "&sfjy=" + encodeURIComponent(sfjy) + "&csrq=" + encodeURIComponent(csrq));
+}
+
 function validateForm() {
-    let yhid = document.getElementById("yhidText").value;
-    let yhxm = document.getElementById('yhxmText').value;
-    let yhkl = document.getElementById('yhklText').value;
-    let cfkl = document.getElementById('cfklText').value;
+    // let yhid = document.getElementById("yhidText").value;
+    let yhid = $("#yhidText").val();
+    // let yhxm = document.getElementById('yhxmText').value;
+    let yhxm = $("#yhxmText").val();
+    // let yhkl = document.getElementById('yhklText').value;
+    let yhkl = $("#yhklText").val();
+    // let cfkl = document.getElementById('cfklText').value;
+    let cfkl = $("#cfklText").val();
+
 
     if (yhid == null || yhid == "") {
         alert("id必须填写");
@@ -20,68 +147,4 @@ function validateForm() {
         alert("重复口令必须填写");
         return false;
     }
-
-}
-
-$(function () {
-    checkInfo();
-    $("#back").click(function () {
-        window.close();
-    });
-})
-
-
-function checkInfo() {
-    let xh = getQueryString("xh");
-    let pxh = getQueryString("pxh");
-    let name = getQueryString("name");
-    let yhkl = getQueryString("yhkl");
-    let yhbm = getQueryString("yhbm");
-    let xb = getQueryString("xb");
-    let sfjy = getQueryString("sfjy");
-    let csrq = getQueryString("csrq");
-
-    if (xh == null || pxh == null || name == null || yhkl == null || yhbm == null || xb == null || sfjy == null || csrq == null) {
-        retun;
-    }
-
-    $("#yhidText").parent().html(xh);
-
-    $("#pxhText").parent().html(pxh);
-    $("#yhxmText").parent().html(name);
-
-    $("#yhklText").parent().html(yhkl);
-    $("#cfklText").parent().html(yhkl);
-
-    $('#yhbm').hide();
-    $("#yhbm").parent().html(yhbm);
-
-    $('#xb').hide();
-    $("#xb").parent().html(xb);
-
-    if (sfjy == '否') {
-        $('#sfjy').hide();
-        $('#sfjy').parent().html('否');
-    } else {
-        $('#sfjy').hide();
-        $('#sfjy').parent().html('是');
-    }
-
-    $('#date').hide();
-    $("#date").parent().html(csrq);
-
-    $('#submit').hide();
-}
-
-function getQueryString(name) {
-    let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
-    let r = window.location.search.substr(1).match(reg);
-    if (r != null) {
-        return decodeURIComponent(r[2]);
-    };
-    return null;
-}
-
-function modifier() {
-    
 }
